2c5b558f7286472685f9eb02479531fea2675b64
Md. Aminul Islam <jonyju@gmail.com>
2025-04-06 01:18:56 +0300

    Booking models added

---
+++ models/Booking.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  carId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Car",
+    required: true
+  },
+  name: String,
+  email: String,
+  startDate: String,
+  endDate: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
