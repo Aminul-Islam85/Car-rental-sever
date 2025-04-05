75a29959dcf0be79e81cb8520d8c66d818ba9b91
Md. Aminul Islam <jonyju@gmail.com>
2025-04-06 00:33:41 +0300

    Something added

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -12,4 +12,14 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+    try {
+      const cars = await Car.find();
+      res.json(cars);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch cars" });
+    }
+  });
+  
+
 module.exports = router;
